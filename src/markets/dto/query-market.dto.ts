b391b42a10d4aa00d66896d774d8c11ed9640d6d
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-07-20 02:06:12 +0500

    docs: improve query-market DTO Swagger description

--- src/markets/dto/query-market.dto.ts
+++ src/markets/dto/query-market.dto.ts
@@ -3,7 +3,7 @@ import { ApiPropertyOptional } from '@nestjs/swagger'
 import { PaginationDto } from '../../common/dto/pagination.dto'
 
 export class QueryMarketDto extends PaginationDto {
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({ description: 'Search by market name or address' })
   @IsOptional()
   @IsString()
   search?: string
