2dbbf681cc58b7f1e5ea3cbf147727bf94aeffb6
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-07-20 02:06:06 +0500

    feat: register StorageService in CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,6 +3,7 @@ import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
 import { AllExceptionsFilter } from './filters/http-exception.filter'
 import { TransformInterceptor } from './interceptors/transform.interceptor'
 import { LoggingInterceptor } from './interceptors/logging.interceptor'
+import { StorageService } from './services/storage.service'
 
 @Global()
 @Module({
@@ -10,7 +11,8 @@ import { LoggingInterceptor } from './interceptors/logging.interceptor'
     { provide: APP_FILTER, useClass: AllExceptionsFilter },
     { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
     { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    StorageService,
   ],
-  exports: [],
+  exports: [StorageService],
 })
 export class CommonModule {}
