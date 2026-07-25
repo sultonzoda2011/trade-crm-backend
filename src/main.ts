026605c24e73ef1d64b806ebcb84021b695732ec
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-07-25 07:51:10 +0500

    feat(api): implement bootstrap function and API handler, add CORS support chore: update .gitignore to include .vercel chore: add vercel.json for deployment configuration

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 	app.enableCors({
 		origin:
 			nodeEnv === 'production'
-				? ['https://your-frontend-domain.com']
+				? ['https://trade-crm.vercel.app']
 				: ['http://localhost:5173', 'http://localhost:3000'],
 		credentials: true,
 		methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
