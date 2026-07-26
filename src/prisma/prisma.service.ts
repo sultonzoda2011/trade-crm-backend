082f37ddf364c5d54506fa53166a7dbcfbf588a1
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-07-26 18:19:45 +0500

    refactor: update build scripts to include Prisma generation and fix import path in Prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from 'prisma/generated/client'
+import { PrismaClient } from '@prisma/client'
 
 const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL!
 })
 
 @Injectable()
