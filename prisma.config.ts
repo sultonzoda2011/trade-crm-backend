8372ffe75e6157f6105e7368f5f14123a2bd8610
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-04 02:50:29 +0500

    refactor: use defineConfig and dotenv in Prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,9 @@
-import type { PrismaConfig } from 'prisma'
+import 'dotenv/config'
+import { defineConfig } from 'prisma/config'
 
-const config: PrismaConfig = {
-  schema: 'prisma/schema.prisma',
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-}
-
-export default config
+export default defineConfig({
+	schema: 'prisma/schema.prisma',
+	datasource: {
+		url: process.env.DATABASE_URL!
+	}
+})
