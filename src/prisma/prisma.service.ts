173e66f70bef8f571f763355fddc7848f63d2a7e
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-04 02:50:42 +0500

    refactor: load dotenv and construct Prisma adapter in constructor

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
-
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL!
-})
+import 'dotenv/config'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleDestroy {
 	constructor() {
-		super({ adapter })
+		const adapter = new PrismaPg({
+			connectionString: process.env.DATABASE_URL!
+		})
+
+		super({
+			adapter
+		})
 	}
 
 	async onModuleDestroy() {
