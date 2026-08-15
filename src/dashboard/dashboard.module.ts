4aaed7906f8a6e68524d226f7ad73cadb7660f1c
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-15 09:29:48 +0500

    feat: register dashboard metrics and insights providers

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { DashboardController } from './dashboard.controller'
+import { DashboardInsightsService } from './dashboard-insights.service'
+import { DashboardMetricsService } from './dashboard-metrics.service'
 import { DashboardService } from './dashboard.service'
 
 @Module({
   controllers: [DashboardController],
-  providers: [DashboardService],
+  providers: [DashboardService, DashboardMetricsService, DashboardInsightsService],
 })
 export class DashboardModule {}
