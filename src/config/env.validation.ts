78149819506b70955bb5f50ff5de534c3a0b3724
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-16 01:13:21 +0500

    refactor: remove JWT_REFRESH_* env validation

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -18,14 +18,11 @@ class EnvironmentVariables {
   @IsString()
   JWT_ACCESS_SECRET: string
 
-  @IsString()
-  JWT_REFRESH_SECRET: string
+ 
 
   @IsString()
   JWT_ACCESS_EXPIRES_IN: string = '15m'
 
-  @IsString()
-  JWT_REFRESH_EXPIRES_IN: string = '7d'
 
   @IsNumber()
   @IsOptional()
