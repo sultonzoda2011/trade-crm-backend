44c1fabbe45e99530c8f5c0b4d2580cf05c1a3dd
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-08-16 01:13:09 +0500

    refactor: remove RefreshDto no longer used

--- src/auth/dto/refresh.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsUUID } from 'class-validator'
-import { ApiProperty } from '@nestjs/swagger'
-
-export class RefreshDto {
-  @ApiProperty()
-  @IsString()
-  @IsUUID()
-  refreshToken: string
-}
