cf7ddad97b7ac7235ff823882d0be23b059dff8f
Sultonzoda Abdulloh <sultonzoda2011@gmail.com>
2026-09-18 06:50:04 +0500

    feat: add MarketsModule and TransactionsModule to UsersModule imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
+import { MarketsModule } from '../markets/markets.module'
+import { TransactionsModule } from '../transactions/transactions.module'
 
 @Module({
+  imports: [MarketsModule, TransactionsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
